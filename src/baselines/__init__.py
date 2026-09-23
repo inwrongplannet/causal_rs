e0bbd211fbc9ba90da7cae6e7c552a0e1ce6cdd0
Abhishek-M-29 <abhishekmurali2006@gmail.com>
2026-09-24 01:44:53 +0530

    feat: scaffold src/baselines package

---
+++ src/baselines/__init__.py
@@ -0,0 +1,7 @@
+from src.baselines.logistic_cf import (
+    build_feature_matrix,
+    train_logistic_cf,
+    score_candidates,
+)
+
+__all__ = ["build_feature_matrix", "train_logistic_cf", "score_candidates"]
